8dad38b4ee73ac04ea86bc8e41210988b9538011
bilashProshad <pbilash64@gmail.com>
2020-07-29 18:24:50 +0600

    Move game object using Rigidbody2d Addforce method

---
+++ Introduction to C# Programming and Unity/02 Move Game Object (AddForce)/Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        //rigidbody2D.AddForce(new Vector2(5, 0), ForceMode2D.Impulse);
+
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(5, 0), ForceMode2D.Impulse);
+    }
+
+    
+}
