8d3b94a0d5e356086e3ac444d06ae83d3edd2057
bilashProshad <pbilash64@gmail.com>
2020-07-29 19:41:05 +0600

    Resizer

---
+++ Introduction to C# Programming and Unity/04 Shrink and Grow/Assets/Scripts/Resizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    // timer
+    const float TotalResizeSeconds = 1f;
+    float elapsedResizeSeconds = 0f;
+
+    const float ScaleFactorPerSeconds = 1f;
+    float scaleFactorMultiplayer = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newScale = transform.localScale;
+        newScale.x += scaleFactorMultiplayer * ScaleFactorPerSeconds * Time.deltaTime;
+        newScale.y += scaleFactorMultiplayer * ScaleFactorPerSeconds * Time.deltaTime;
+        transform.localScale = newScale;
+        
+        elapsedResizeSeconds += Time.deltaTime;
+        if(elapsedResizeSeconds >= TotalResizeSeconds)
+        {
+            scaleFactorMultiplayer *= -1;
+            elapsedResizeSeconds = 0;
+        }
+
+        
+    }
+}
