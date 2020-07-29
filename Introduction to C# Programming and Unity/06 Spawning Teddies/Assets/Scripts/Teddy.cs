816a9766383a5f55827e02442b13d501336089ba
bilashProshad <pbilash64@gmail.com>
2020-07-29 21:53:14 +0600

    Spawn game object in time

---
+++ Introduction to C# Programming and Unity/06 Spawning Teddies/Assets/Scripts/Teddy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teddy : MonoBehaviour
+{
+    // death support
+    Timer deathTimer;
+    const float TeddyBearLifeSpanSeconds = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        const float MinImpolseForce = 3f;
+        const float MaxImpolseForce = 5f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        float magnitude = Random.Range(MinImpolseForce, MaxImpolseForce);
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        GetComponent<Rigidbody2D>().AddForce(magnitude * direction, ForceMode2D.Impulse);
+
+        deathTimer = gameObject.AddComponent<Timer>();
+        deathTimer.Duration = TeddyBearLifeSpanSeconds;
+        deathTimer.Run();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (deathTimer.Finished)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Introduction to C# Programming and Unity/06 Spawning Teddies/Assets/Scripts/TeddyBearSpawner.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeddyBearSpawner : MonoBehaviour
+{
+    // prefab for spawn
+    [SerializeField]
+    GameObject prefabTeddyBear;
+    
+    // sprite for change randerar
+    [SerializeField]
+    Sprite teddyBearSprite0;
+    [SerializeField]
+    Sprite teddyBearSprite1;
+    [SerializeField]
+    Sprite teddyBearSprite2;
+
+    // support for timer
+    const float MinSpawnTime = 1f;
+    const float MaxSpawnTime = 2f;
+    Timer spawnTimer;
+
+    // spawn border area
+    const int SpawnborderSize = 100;
+    int minSpawnX;
+    int maxSpawnX;
+    int minSpawnY;
+    int maxSpawnY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // save spawn boundaries
+        minSpawnX = SpawnborderSize;
+        maxSpawnX = Screen.width - SpawnborderSize;
+        minSpawnY = SpawnborderSize;
+        maxSpawnY = Screen.height - SpawnborderSize;
+
+        // start and save timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = Random.Range(MinSpawnTime, MaxSpawnTime);
+        spawnTimer.Run();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer.Finished)
+        {
+            SpawnBear();
+
+            spawnTimer.Duration = Random.Range(MinSpawnTime, MaxSpawnTime);
+            spawnTimer.Run();
+        }
+    }
+
+    void SpawnBear()
+    {
+        Vector3 location = new Vector3
+            (Random.Range(minSpawnX, maxSpawnX), 
+            Random.Range(minSpawnY, maxSpawnY), 
+            -Camera.main.transform.position.z);
+
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+
+        GameObject teddyBear = Instantiate(prefabTeddyBear) as GameObject;
+        teddyBear.transform.position = worldLocation;
+
+        SpriteRenderer spriteRenderer = teddyBear.GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, 3);
+        if(spriteNumber == 0)
+        {
+            spriteRenderer.sprite = teddyBearSprite0;
+        }
+        else if(spriteNumber == 1)
+        {
+            spriteRenderer.sprite = teddyBearSprite1;
+        }
+        else
+        {
+            spriteRenderer.sprite = teddyBearSprite2;
+        }
+    }
+}
