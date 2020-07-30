116eb0f2ca2bf4949047c83ec9e5a3f481fea798
bilashProshad <pbilash64@gmail.com>
2020-07-30 22:02:36 +0600

    move gameobject using mouse and clamp gameobject in screen

---
+++ Introduction to C# Programming and Unity/10 Mouse Input and Clampdown/Assets/Scripts/Character.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // half collider
+    float colliderHalfHeight;
+    float colliderHalfWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+        colliderHalfHeight = boxCollider2D.size.y / 2;
+        colliderHalfWidth = boxCollider2D.size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = Input.mousePosition;
+        position.z = -Camera.main.transform.position.z;
+        position = Camera.main.ScreenToWorldPoint(position);
+        transform.position = position;
+
+        ClampScreen();
+    }
+
+    void ClampScreen()
+    {
+        Vector3 position = transform.position;
+
+        if(position.x - colliderHalfWidth < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + colliderHalfWidth;
+        }
+        else if(position.x + colliderHalfWidth > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenRight - colliderHalfWidth;
+        }
+        if(position.y + colliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenTop - colliderHalfHeight;
+        }
+        else if(position.y - colliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenBottom + colliderHalfHeight;
+        }
+
+        transform.position = position;
+    }
+}
