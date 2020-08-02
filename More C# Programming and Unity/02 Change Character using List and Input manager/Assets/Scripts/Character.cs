b699e1a5ef6d3b0686d0b386c2db1a708ee21ec4
bilashProshad <pbilash64@gmail.com>
2020-08-02 22:41:48 +0600

    change character using list and add game object into list dynamically

---
+++ More C# Programming and Unity/02 Change Character using List and Input manager/Assets/Scripts/Character.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    float colliderHalfWidth;
+    float colliderHalfHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+        colliderHalfHeight = boxCollider2D.size.y / 2;
+        colliderHalfWidth = boxCollider2D.size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 postion = Input.mousePosition;
+        postion = Camera.main.ScreenToWorldPoint(postion);
+        transform.position = postion;
+
+        ClampInScreen();
+    }
+
+    void ClampInScreen()
+    {
+        Vector3 position = transform.position;
+
+        if(position.x - colliderHalfWidth < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + colliderHalfWidth;
+        }
+        else if(position.x + colliderHalfWidth > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenRight - colliderHalfWidth;
+        }
+        if(position.y - colliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenBottom + colliderHalfHeight;
+        }
+        else if(position.y + colliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenTop - colliderHalfHeight;
+        }
+
+        transform.position = position;
+    }
+}
---
+++ More C# Programming and Unity/02 Change Character using List and Input manager/Assets/Scripts/CharacterChanger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterChanger : MonoBehaviour
+{
+    //[SerializeField]
+    //GameObject[] prefabCharacters;
+
+    List<GameObject> prefabCharacters = new List<GameObject>(); // dinamically add
+
+    GameObject currnetCharacter;
+    bool previousFrameCharacterChangeFlag = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // dinamically add gameobject
+        prefabCharacters.Add((GameObject)Resources.Load(@"Prefabs\character0"));
+        prefabCharacters.Add((GameObject)Resources.Load(@"Prefabs\character1"));
+        prefabCharacters.Add((GameObject)Resources.Load(@"Prefabs\character2"));
+        prefabCharacters.Add((GameObject)Resources.Load(@"Prefabs\character3"));
+
+        currnetCharacter = Instantiate<GameObject>(prefabCharacters[0], Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("CharacterChanger") > 0)
+        {
+            if (!previousFrameCharacterChangeFlag)
+            {
+                previousFrameCharacterChangeFlag = true;
+
+                Vector3 position = currnetCharacter.transform.position;
+                Destroy(currnetCharacter);
+
+                currnetCharacter = Instantiate<GameObject>(
+                    prefabCharacters[Random.Range(0, prefabCharacters.Count)], 
+                    position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            previousFrameCharacterChangeFlag = false;
+        }
+    }
+}
